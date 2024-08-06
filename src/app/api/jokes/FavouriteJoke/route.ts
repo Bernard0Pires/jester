2d59e46a604d2f6eb7d537fa8e297f2c85f90b5f
Bernardo Pires <bernardopires302@gmail.com>
2024-08-06 19:55:29 +0100

    supabase
    favorite joke is being added to table   missing the delete and select options

--- src/app/api/jokes/FavouriteJoke/route.ts
+++ src/app/api/jokes/FavouriteJoke/route.ts
@@ -6,46 +6,29 @@ export async function PUT(req: Request){
         const supabase = await supabaseServerClient();
                
         const user = await supabase.auth.getUser();
+        
         if (!user) {
             return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
         }
 
         const joke = await req.json();
-
-        const { error } = await supabase
+              
+        const { data, error } = await supabase
             .from('saved_jokes')
-            .insert([
+            .insert(
                 {
-                    save_joke_id: joke.id,
-                    save_user: user.data.user?.id  
-                }
-            ]);
+                    save_joke_id: joke.joke.id,
+                    save_user: user.data.user?.id,
+                },
+            )
+            .select()
+        
+        console.log(data)
+        console.log(error)
+
         return NextResponse.json({ message: 'Joke added to favorites successfully' });
     } catch (error) {
         console.error('Error handling the request:', error);
         return NextResponse.json({ error: 'Failed to process request' }, { status: 500 });
     }
-}
-
-export async function DELETE(req: Request){
-    const {searchParams} = new URL(req.url)
-    let ApiUrl = "2"
-    const categorys = searchParams.get('categorys')
-
-    if(categorys && categorys.trim() !== ''){
-        let requiredCategorys : string[] = categorys.split(',');
-        requiredCategorys.forEach((category, index)=>{
-            ApiUrl += category
-            
-            if(index < requiredCategorys.length -1){
-                ApiUrl += ","
-            }
-        })
-    } else {
-        ApiUrl += "Any"
-    }
-
-   const res = await fetch(ApiUrl)
-   const data = await res.json();
-    return NextResponse.json(data)
 }
